5f03b4e5ec6ca26683690b09c12fbb75e4801fbc
Turtlemint-QA Team <64896322+tm-qa@users.noreply.github.com>
2025-06-26 16:32:44 +0530

    setup branch

--- src/main/java/Base/TestBase.java
+++ src/main/java/Base/TestBase.java
@@ -8,28 +8,30 @@ import java.net.URL;
 
 public class TestBase {
 
-   public static AndroidDriver driver;
+    public static AndroidDriver driver;
 
-   public static void TurtlemintProApp() throws MalformedURLException, InterruptedException {
-      System.out.println("started suite");
-      DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    public static void TurtlemintProApp() throws MalformedURLException, InterruptedException {
+        System.out.println("started suite");
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
-      desiredCapabilities.setCapability("deviceName", "Pixel 7");
-      desiredCapabilities.setCapability("udid", "emulator-5554");
-      desiredCapabilities.setCapability("platformName", "Android");
-      desiredCapabilities.setCapability("platformVersion", "12");
+        desiredCapabilities.setCapability("deviceName", "Pixel 7");
+        desiredCapabilities.setCapability("udid", "emulator-5554");
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("platformVersion", "12");
 
 //        desiredCapabilities.setCapability("noReset", "true");
 //        desiredCapabilities.setCapability("fullReset", "false");
-      desiredCapabilities.setCapability("automationName", "UiAutomator2");
-     //               desiredCapabilities.setCapability("app", "/Users/tejasbahadure/Downloads/app-release.apk");
-      desiredCapabilities.setCapability("appPackage", "com.turtlemintconsumerapp");
-      desiredCapabilities.setCapability("appActivity", "com.turtlemintconsumerapp.MainActivity");
+        desiredCapabilities.setCapability("automationName", "UiAutomator2");
+        //                  desiredCapabilities.setCapability("app", "/home/ubuntu/storage/consumer.apk");
+        desiredCapabilities.setCapability("app", "/home/ubuntu/storage/prodconsumer.apk");
+
+        desiredCapabilities.setCapability("appPackage", "com.turtlemintconsumerapp");
+        desiredCapabilities.setCapability("appActivity", "com.turtlemintconsumerapp.MainActivity");
 
 
-      URL url = new URL("http://127.0.0.1:4723/");
+        URL url = new URL("http://127.0.0.1:4723/");
 
-      driver = new AndroidDriver(url, desiredCapabilities);
-      System.out.println("Application Start");
-   }
+        driver = new AndroidDriver(url, desiredCapabilities);
+        System.out.println("Application Start");
+    }
 }
