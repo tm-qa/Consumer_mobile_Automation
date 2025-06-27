62ff3889cb942036535def6d520137c02292718d
Turtlemint-QA Team <64896322+tm-qa@users.noreply.github.com>
2025-06-27 11:15:01 +0530

    url change

--- src/main/java/Base/TestBase.java
+++ src/main/java/Base/TestBase.java
@@ -1,6 +1,7 @@
 package Base;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -10,34 +11,65 @@ public class TestBase {
 
     public static AndroidDriver driver;
 
-    public static void TurtlemintProApp() throws MalformedURLException, InterruptedException {
-        System.out.println("started suite");
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//    public static void TurtlemintProApp() throws MalformedURLException, InterruptedException {
+//        System.out.println("started suite");
+//        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//
+//        desiredCapabilities.setCapability("deviceName", "Pixel 7");
+//        desiredCapabilities.setCapability("udid", "emulator-5554");
+//        desiredCapabilities.setCapability("platformName", "Android");
+//        desiredCapabilities.setCapability("platformVersion", "12");
+//
+////        desiredCapabilities.setCapability("noReset", "true");
+////        desiredCapabilities.setCapability("fullReset", "false");
+//        desiredCapabilities.setCapability("automationName", "UiAutomator2");
+//        //                  desiredCapabilities.setCapability("app", "/home/ubuntu/storage/consumer.apk");
+//        desiredCapabilities.setCapability("app", "/home/ubuntu/storage/prodconsumer.apk");
+//
+//        desiredCapabilities.setCapability("appPackage", "com.turtlemintconsumerapp");
+//        desiredCapabilities.setCapability("appActivity", "com.turtlemintconsumerapp.MainActivity");
+//
+//
+//        URL url = new URL("http://127.0.0.1:4723/");
+//
+//
+//     //   URL url =   new URL("http://127.0.0.1:4723/wd/hub");
+//
+//     //   driver = new AndroidDriver(new URL("http://localhost:4723/"), desiredCapabilities);
+//
+//
+//        driver = new AndroidDriver(url, desiredCapabilities);
+//        System.out.println("Application Start");
+//    }
 
-        desiredCapabilities.setCapability("deviceName", "Pixel 7");
-        desiredCapabilities.setCapability("udid", "emulator-5554");
-        desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("platformVersion", "12");
+        public static void TurtlemintProApp() throws MalformedURLException, InterruptedException {
+        System.out.println("Starting Appium session for Turtlemint Consumer App...");
 
-//        desiredCapabilities.setCapability("noReset", "true");
-//        desiredCapabilities.setCapability("fullReset", "false");
-        desiredCapabilities.setCapability("automationName", "UiAutomator2");
-        //                  desiredCapabilities.setCapability("app", "/home/ubuntu/storage/consumer.apk");
-        desiredCapabilities.setCapability("app", "/home/ubuntu/storage/prodconsumer.apk");
+        UiAutomator2Options options = new UiAutomator2Options();
 
-        desiredCapabilities.setCapability("appPackage", "com.turtlemintconsumerapp");
-        desiredCapabilities.setCapability("appActivity", "com.turtlemintconsumerapp.MainActivity");
+        options.setDeviceName("Pixel 7");
+        options.setUdid("emulator-5554");
+        options.setPlatformName("Android");
+        options.setPlatformVersion("12");
 
+        // Appium 2.x requires fully qualified path to APK
+        options.setApp("/home/ubuntu/storage/prodconsumer.apk");
 
-      //  URL url = new URL("http://127.0.0.1:4723/");
+        options.setAppPackage("com.turtlemintconsumerapp");
+        options.setAppActivity("com.turtlemintconsumerapp.MainActivity");
 
+        options.setAutomationName("UiAutomator2");
 
-     //   URL url =   new URL("http://127.0.0.1:4723/wd/hub");
+        // Optional capabilities
+        // options.setNoReset(true);
+        // options.setFullReset(false);
 
-        driver = new AndroidDriver(new URL("http://localhost:4723/"), desiredCapabilities);
+        // Appium server URL (Jenkins server where Appium is running)
+        URL url = new URL("http://127.0.0.1:4723/");
 
+        // Initialize the AndroidDriver (this automatically uses W3C compatible request for Appium 2.x)
+        driver = new AndroidDriver(url, options);
 
-      //  driver = new AndroidDriver(url, desiredCapabilities);
-        System.out.println("Application Start");
+        System.out.println("Appium session started. App launched successfully!");
     }
 }
